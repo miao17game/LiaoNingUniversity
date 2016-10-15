57ba50859aa9b5f30837d771b6deba4c46a3894b
Wallace Sun <245396954@qq.com>
2016-10-15 14:30:06 +0800

    add project

---
+++ Helpers/Tools/DataProcess.cs
@@ -0,0 +1,38 @@
+﻿using DBCSCodePage;
+using Wallace.UWP.Helpers.Controls;
+using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media.Imaging;
+
+using static Wallace.UWP.Helpers.Tools.UWPStates;
+
+namespace Wallace.UWP.Helpers.Tools {
+    public static class DataProcess {
+        #region Properties and State
+        
+        #endregion
+
+        public static async void ReportError(string erroeMessage) {
+            await Window.Current.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => {
+                new ToastSmoothBase("Fetch Data Error \n" +erroeMessage).Show();
+            });
+        }
+
+        public static async void ReportException(string erroeMessage) {
+            await Window.Current.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => {
+                new ToastSmoothBase(erroeMessage).Show();
+            });
+        }
+
+        public static Uri ConvertToUri(string str) { return !string.IsNullOrEmpty(str) ? new Uri(str) : null; }
+        
+    }
+}
